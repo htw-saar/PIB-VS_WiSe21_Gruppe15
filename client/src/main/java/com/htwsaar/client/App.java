4dce12655b1aa0be3dc05a16c4d4eb2ccd47b780
Simon <schmitt-simon@outlook.de>
2022-01-16 20:09:29 +0100

    New maven projects for client and server

---
+++ client/src/main/java/com/htwsaar/client/App.java
@@ -0,0 +1,13 @@
+package com.htwsaar.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from client!" );
+    }
+}
---
+++ server/src/main/java/com/htwsaar/server/App.java
@@ -0,0 +1,13 @@
+package com.htwsaar.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from Server!" );
+    }
+}
