4dfd083c645a13802d8b8d246a8d49fac056d0da
Simon <schmitt-simon@outlook.de>
2022-01-16 20:24:55 +0100

    start working on the menu for the client

---
+++ client/src/main/java/com/htwsaar/client/UserInterface/Menu/Menu.java
@@ -0,0 +1,7 @@
+package com.htwsaar.client.UserInterface.Menu;
+
+public class Menu {
+    public static void startMenu(){
+        System.out.println( "This will be the menu for the user" );
+    }
+}
