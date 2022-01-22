3b12b83fd258f1000eba4c1f5dfef0bef8d4db2f
Simon <schmitt-simon@outlook.de>
2022-01-22 22:22:50 +0100

    New error handling starting with the database

--- server/src/main/java/com/htwsaar/server/App.java
+++ server/src/main/java/com/htwsaar/server/App.java
@@ -2,25 +2,14 @@ package com.htwsaar.server;
 
 import com.htwsaar.server.Database.DatabaseService;
 
-import java.sql.SQLException;
-
 public class App {
     public static void main(String[] args) {
         start();
     }
 
     private static void start() {
-        try {
-            DatabaseService databaseService = new DatabaseService();
-            databaseService.run();
-//            databaseService.addLoses(1);
-            databaseService.changePassword(1,"s","test");
-        } catch (SQLException ex) {
-            System.out.println("FEHLER (SQLException): " + ex.getMessage());
-        } catch (IllegalArgumentException ex) {
-            System.out.println("FEHLER (IllegalArgumentException): " + ex.getMessage());
-        } catch (Exception ex) {
-            System.out.println("FEHLER: " + ex.getMessage());
-        }
+        DatabaseService databaseService = new DatabaseService();
+        // databaseService.addLoses(1);
+        databaseService.changePassword(1, "s", "test");
     }
 }
--- server/src/main/java/com/htwsaar/server/Database/DatabaseService.java
+++ server/src/main/java/com/htwsaar/server/Database/DatabaseService.java
@@ -7,15 +7,20 @@ public class DatabaseService {
     private final String DB_URL = "jdbc:mysql://localhost/gameServer";
     private final String USER = "root";
     private final String PASS = "test";
-    private final Connection con;
-    private final Statement stmt;
+    private Connection con;
+    private Statement stmt;
 
-    public DatabaseService() throws SQLException {
-        con = DriverManager.getConnection(DB_URL, USER, PASS);
-        stmt = con.createStatement();
+    public DatabaseService() {
+        try {
+            initDatabase();
+        } catch (SQLException e) {
+            handleError(e);
+        }
     }
 
-    public void run() throws SQLException {
+    private void initDatabase() throws SQLException {
+        con = DriverManager.getConnection(DB_URL, USER, PASS);
+        stmt = con.createStatement();
         if (!tableExists("user")) {
             createTable();
         }
@@ -23,66 +28,108 @@ public class DatabaseService {
 
     private boolean tableExists(String tableName) throws SQLException {
         DatabaseMetaData meta = con.getMetaData();
-        ResultSet resultSet = meta.getTables(null, null, tableName, new String[]{"TABLE"});
+        ResultSet resultSet = meta.getTables(null, null, tableName, new String[] { "TABLE" });
 
         return resultSet.next();
     }
 
-    private void createTable() throws SQLException {
-        String sql = "CREATE TABLE user(UserID int primary key auto_increment, Username varchar(255) UNIQUE ,Password varchar(255) NOT NULL, Wins int, Loses int, Games int)";
-        stmt.executeUpdate(sql);
-    }
-
-    public void addUser(String username, String password) throws SQLException {
-        if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
-            throw new IllegalArgumentException();
+    private void executeUpdate(String command) {
+        try {
+            stmt.executeUpdate(command);
+        } catch (SQLException e) {
+            handleError(e);
         }
-        String sql = "INSERT INTO user(Username, Password, Wins, Loses, Games) VALUES ('" + username + "', '" + password + "', 0, 0, 0)";
-        stmt.executeUpdate(sql);
+
     }
 
-    public void addWins(int userID) throws SQLException {
-        ResultSet rs = stmt.executeQuery("SELECT Wins, Games FROM user WHERE UserID = " + userID);
-        if (rs.next()) {
-            int wins = rs.getInt("Wins") + 1;
-            int games = rs.getInt("Games") + 1;
+    private void handleError(Exception error) {
+        // TODO: handle exception
+        System.out.println(error);
+    }
 
-            String sql = "UPDATE user SET Wins = " + wins + ", Games = " + games + " WHERE UserID = " + userID;
+    /*
+     * -----------------------------------------------------------------------------
+     * -------------
+     */
+
+    private void createTable() {
+        String command = "CREATE TABLE user(UserID int primary key auto_increment, Username varchar(255) UNIQUE ,Password varchar(255) NOT NULL, Wins int, Loses int, Games int)";
+        executeUpdate(command);
+    }
+
+    public void addUser(String username, String password) {
+        try {
+            if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            String sql = "INSERT INTO user(Username, Password, Wins, Loses, Games) VALUES ('" + username + "', '"
+                    + password + "', 0, 0, 0)";
             stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            handleError(e);
         }
+
     }
 
-    public void addLoses(int userID) throws SQLException {
-        ResultSet rs = stmt.executeQuery("SELECT Loses, Games FROM user WHERE UserID = " + userID);
-        if (rs.next()) {
-            int loses = rs.getInt("Loses") + 1;
-            int games = rs.getInt("Games") + 1;
+    public void addWins(int userID) {
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT Wins, Games FROM user WHERE UserID = " + userID);
+            if (rs.next()) {
+                int wins = rs.getInt("Wins") + 1;
+                int games = rs.getInt("Games") + 1;
 
-            String sql = "UPDATE user SET Loses = " + loses + ", Games = " + games + " WHERE UserID = " + userID;
-            stmt.executeUpdate(sql);
-        }
-    }
-
-    public void changePassword(int userID, String oldPassword, String newPassword) throws SQLException {
-        ResultSet rs = stmt.executeQuery("SELECT Password FROM user WHERE UserID = " + userID);
-        if (rs.next()) {
-            String pw = rs.getString("Password");
-
-            if (oldPassword.equals(pw)) {
-                String sql = "UPDATE user SET Password = '" + newPassword + "' WHERE UserID = " + userID;
+                String sql = "UPDATE user SET Wins = " + wins + ", Games = " + games + " WHERE UserID = " + userID;
                 stmt.executeUpdate(sql);
             }
+        } catch (SQLException e) {
+            handleError(e);
         }
     }
 
-    public void getScoreboard() throws SQLException {
-        ResultSet rs = stmt.executeQuery("SELECT Username, Wins, Loses FROM user ORDER BY Games, Wins DESC");
-        while (rs.next()) {
-            String username = rs.getString("Username");
-            int wins = rs.getInt("Wins");
-            int loses = rs.getInt("Loses");
+    public void addLoses(int userID) {
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT Loses, Games FROM user WHERE UserID = " + userID);
+            if (rs.next()) {
+                int loses = rs.getInt("Loses") + 1;
+                int games = rs.getInt("Games") + 1;
 
-            System.out.println(username + ": " + wins + " - " + loses);
+                String sql = "UPDATE user SET Loses = " + loses + ", Games = " + games + " WHERE UserID = " + userID;
+                stmt.executeUpdate(sql);
+            }
+        } catch (SQLException e) {
+            handleError(e);
+        }
+    }
+
+    public void changePassword(int userID, String oldPassword, String newPassword) {
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT Password FROM user WHERE UserID = " + userID);
+            if (rs.next()) {
+                String pw = rs.getString("Password");
+
+                if (oldPassword.equals(pw)) {
+                    String sql = "UPDATE user SET Password = '" + newPassword + "' WHERE UserID = " + userID;
+                    stmt.executeUpdate(sql);
+                }
+            }
+        } catch (SQLException e) {
+            handleError(e);
+        }
+    }
+
+    public void getScoreboard() {
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT Username, Wins, Loses FROM user ORDER BY Games, Wins DESC");
+            while (rs.next()) {
+                String username = rs.getString("Username");
+                int wins = rs.getInt("Wins");
+                int loses = rs.getInt("Loses");
+
+                System.out.println(username + ": " + wins + " - " + loses);
+            }
+            // TODO return users as array of users
+        } catch (SQLException e) {
+            handleError(e);
         }
     }
 }
