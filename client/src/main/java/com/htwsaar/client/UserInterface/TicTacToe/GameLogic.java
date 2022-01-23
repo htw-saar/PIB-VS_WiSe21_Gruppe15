804aa4360edb7bd12adc169abbb40478f55c2e23
Hamudi <AhmadAnwar@web.de>
2022-01-23 17:44:02 +0100

    @ GameBoard Print Nach Definierung des Gameboards, Methode zur Ausgabe auf dem Terminal

--- client/src/main/java/com/htwsaar/client/UserInterface/TicTacToe/GameLogic.java
+++ client/src/main/java/com/htwsaar/client/UserInterface/TicTacToe/GameLogic.java
@@ -1,10 +1,30 @@
 package com.htwsaar.client.UserInterface.TicTacToe;
 
 public class GameLogic {
-    char[][] gameBoard = {{' ', '|', ' ', '|', ' '},            //Gameboard Array 3 Reihen 3 Spalten, 3x3 Matrix
-            {'-', '+', '-', '+', '-'},
-            {' ', '|', ' ', '|', ' '},
-            {'-', '+', '-', '+', '-'},
-            {' ', '|', ' ', '|', ' '}
-    };
+      public static void main(String[] args) {
+
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},            //Gameboard Array 3 Reihen 3 Spalten, 3x3 Matrix
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}
+        };
+        printGameBoard(gameBoard);// Game Board ins Terminal Printen
+
+
+
+
+    }
+    public static void printGameBoard(char [] [] gameBoard ){
+        for (char[] row : gameBoard) { // for each Schleife für jeweilige Reihe in GameBoard
+            for (char c : row) { // for each Character an der jeweiligen Stelle
+                System.out.print(c);
+
+            }
+            System.out.println();
+
+        }
+
+    }
 }
