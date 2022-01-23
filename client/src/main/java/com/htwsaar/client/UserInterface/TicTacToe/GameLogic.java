e6dbdca23885b5d35776b8614ebde1c8aad9e29e
Hamudi <AhmadAnwar@web.de>
2022-01-23 18:29:58 +0100

    @ GameBoard Symbol setzen Methode definiert um ein Symbol zu setzen. Über einen switch case Block wird ermittelt wo das Symbol gesetzt werden soll. Spieler Initialisierung ist noch unvollständig.

--- client/src/main/java/com/htwsaar/client/UserInterface/TicTacToe/GameLogic.java
+++ client/src/main/java/com/htwsaar/client/UserInterface/TicTacToe/GameLogic.java
@@ -1,5 +1,7 @@
 package com.htwsaar.client.UserInterface.TicTacToe;
 
+import java.util.Scanner;
+
 public class GameLogic {
       public static void main(String[] args) {
 
@@ -10,12 +12,24 @@ public class GameLogic {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}
         };
-        printGameBoard(gameBoard);// Game Board ins Terminal Printen
+        printGameBoard(gameBoard);                                  // Game Board ins Terminal Printen
+
+          Scanner scan = new Scanner(System.in);                    // Scanner Anlegen
+          System.out.println("Trage Ziffern ein zwischen 1 und 9:");
+          int pos = scan.nextInt();                                 // Kästschen auswählen
+          System.out.println(pos);
+
+          placeSymb(gameBoard,"player1",pos);
+
+          printGameBoard(gameBoard);
 
 
 
 
-    }
+
+
+
+      }
     public static void printGameBoard(char [] [] gameBoard ){
         for (char[] row : gameBoard) { // for each Schleife für jeweilige Reihe in GameBoard
             for (char c : row) { // for each Character an der jeweiligen Stelle
@@ -27,4 +41,50 @@ public class GameLogic {
         }
 
     }
+    public static void placeSymb(char[][] gameBoard, String user, int pos){  // Methode zum Zeichen setzen, kriegt GameBoard User und Position mit
+
+          char symbol = ' ';
+
+          if(user.equals("player1")) {  // Spielerunterscheidung ob ein X oder ein O gesetzt wird
+              symbol = 'X';
+
+          }else if(user.equals("player2")) {
+                  symbol = 'O';
+              }
+
+
+        switch (pos) {  // Switch Case Block der an der jeweiligen Stelle die mitgegeben wird das Symbol des jeweiligen Spielers einträgt
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+            default:
+                System.out.println("Bitte Zahl zwischen 1 und 9 eingeben!");
+                break;
+        }
+    }
+
 }
