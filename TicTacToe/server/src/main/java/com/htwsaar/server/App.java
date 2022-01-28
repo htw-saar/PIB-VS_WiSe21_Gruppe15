12dbf1a5aeacd723ad2c1e9e53ec9cb9556f4238
Simon <schmitt-simon@outlook.de>
2022-01-28 14:06:08 +0100

    merge Develop into branch

--- TicTacToe/server/src/main/java/com/htwsaar/server/App.java
+++ TicTacToe/server/src/main/java/com/htwsaar/server/App.java
@@ -3,7 +3,6 @@ package com.htwsaar.server;
 import com.htwsaar.server.hibernate.dao.UserDao;
 import com.htwsaar.server.hibernate.entity.User;
 
-import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -23,23 +22,5 @@ public class App {
         } else {
             System.out.println(get.toString());
         }
-
-//        List<User> scoreboard = userDao.getScoreboard();
-//        scoreboard.forEach(users -> System.out.println(users.toString()));
-
-//         Legacy Database Stuff
-//        DatabaseService databaseService = new DatabaseService();
-//        databaseService.addUser("Simon", "test");
-//        databaseService.addUser("Alex", "test");
-
-//         databaseService.addWins(1);
-//         databaseService.addLoses(1);
-//        databaseService.addLoses(2);
-//        databaseService.addWins(2);
-        //databaseService.changePassword(1, "s", "test");
-//        User[] users = databaseService.getScoreboard();
-//        for (int i=0; i<users.length; i++){
-//            System.out.println(users[i]);
-//        }
     }
 }
