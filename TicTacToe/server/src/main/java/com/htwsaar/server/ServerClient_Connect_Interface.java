3374b0bc853199d93c63b33ec2291a1b7a7cc50e
IsAlreadyTaken <Maximilian.Weyand@gmx.de>
2022-01-29 20:17:58 +0100

    Added ServerClient_Connect_Interface Interface(without Functions)
    Added ServerClient_Connect_Interface Functions(return value 0-1, 1 = sucess 0 = not sucess)

--- TicTacToe/server/src/main/java/com/htwsaar/server/ServerClient_Connect_Interface.java
+++ TicTacToe/server/src/main/java/com/htwsaar/server/ServerClient_Connect_Interface.java
@@ -4,4 +4,17 @@ import java.rmi.RemoteException;
 
     public interface ServerClient_Connect_Interface extends Remote {
 
+        int sendLoginData(String name, String password) throws RemoteException;
+
+        int ScoreboardRequest() throws RemoteException;
+
+        int ScoreboardRequestForUser(String name) throws RemoteException;
+
+        int createGame() throws  RemoteException;
+
+        int joinGame(int joinCode) throws RemoteException;
+
+        int setX(int x, int y) throws RemoteException;
+
+        int setO(int x, int y) throws RemoteException;
     }
