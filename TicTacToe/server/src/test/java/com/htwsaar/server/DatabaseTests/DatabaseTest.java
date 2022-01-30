a03ff379733a6c7b31e40136c856eff279069247
Simon <schmitt-simon@outlook.de>
2022-01-30 10:31:18 +0100

    add some testcases to databaseTest

--- TicTacToe/server/src/test/java/com/htwsaar/server/DatabaseTests/DatabaseTest.java
+++ TicTacToe/server/src/test/java/com/htwsaar/server/DatabaseTests/DatabaseTest.java
@@ -18,53 +18,63 @@ public class DatabaseTest
 {
     private DatabaseService databaseService = new DatabaseService();;
     private List<User> testUserList;
-    private String username = "Simon";
-    private String password = "test";
-    private String username2 = "Simon2";
-    private String password2 = "test";
-    private String username3 = "Simon3";
-    private String password3 = "test";
+    private User testuser1 = new User("Simon", "test");
+    private User testuser2 = new User("Simon2", "test");
+    private User testuser3 = new User("Simon3", "test");
+
+    private String changePassword = "testchange";
+
+
 
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void DatabaseAddUsers()
+    public void CheckAddUsers()
     {
-        databaseService.addUser(username, password);
-        databaseService.addUser(username2, password2);
-        databaseService.addUser(username3, password3);
-
+        databaseService.addUser(testuser1.getUsername(), testuser1.getPassword());
+        databaseService.addUser(testuser2.getUsername(), testuser2.getPassword());
+        databaseService.addUser(testuser3.getUsername(), testuser3.getPassword());
     }
-    
+
+
     @Test
-    public void DatabaseGetScoreboard(){
-        databaseService.addWin(username);
-        databaseService.addWin(username);
-        databaseService.addLose(username);
+    public void CheckGetScoreboard(){
+        databaseService.addWin(testuser1.getUsername());
+        databaseService.addWin(testuser1.getUsername());
+        databaseService.addLose(testuser1.getUsername());
 
-        databaseService.addWin(username2);
-        databaseService.addLose(username2);
-        databaseService.addLose(username2);
+        databaseService.addWin(testuser2.getUsername());
+        databaseService.addLose(testuser2.getUsername());
+        databaseService.addLose(testuser2.getUsername());
 
-        databaseService.addWin(username3);
-        databaseService.addWin(username3);
-        databaseService.addWin(username3);
+        databaseService.addWin(testuser3.getUsername());
+        databaseService.addWin(testuser3.getUsername());
+        databaseService.addWin(testuser3.getUsername());
 
         testUserList = new ArrayList<>();
-        testUserList.add(databaseService.getUserData(username3));
-        testUserList.add(databaseService.getUserData(username));
-        testUserList.add(databaseService.getUserData(username2));
+        testUserList.add(databaseService.getUserData(testuser3.getUsername()));
+        testUserList.add(databaseService.getUserData(testuser1.getUsername()));
+        testUserList.add(databaseService.getUserData(testuser2.getUsername()));
 
         List<User> users = databaseService.getScoreboard();
 
         for (int i = 0; i < testUserList.size(); i++) {
             assertEquals(testUserList.get(i).getScore(), users.get(i).getScore());
         }
-
-
     }
 
 
-    
+    @Test
+    public void CheckChangePassword(){
+        databaseService.changePassword(testuser1.getUsername(), testuser1.getPassword(), changePassword);
+        User user = databaseService.getUserData(testuser1.getUsername());
+        assertEquals(changePassword, user.getPassword());
+        databaseService.changePassword(testuser1.getUsername(), changePassword, testuser1.getPassword());
+    }
+
+    @Test
+    public void clearDatabase(){
+//        databaseService.
+    }
 }
