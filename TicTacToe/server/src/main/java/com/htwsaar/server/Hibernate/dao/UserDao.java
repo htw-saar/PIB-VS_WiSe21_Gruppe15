dbeda9f7ed5bc190a4d2ad26bfdce89e5e3ce5b1
Bricker17 <qhtrrA7EBaxCm7Z>
2022-02-03 20:25:29 +0100

    ADDED JavaDoc for UserDao

--- TicTacToe/server/src/main/java/com/htwsaar/server/Hibernate/dao/UserDao.java
+++ TicTacToe/server/src/main/java/com/htwsaar/server/Hibernate/dao/UserDao.java
@@ -7,9 +7,17 @@ import org.hibernate.Transaction;
 
 import java.util.List;
 
+/**
+ * Dao Klasse die Methoden enthält, um Datenbank Änderungen zu vereinfachen
+ */
 public class UserDao {
     Transaction transaction = null;
 
+    /**
+     * Methode um einen neuen Nutzer zu speichern
+     *
+     * @param user Ein Nutzer, der in die Datenbank gespeichert werden soll
+     */
     public void saveUser(User user) {
         try (Session session = HibernateUtils.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
@@ -23,6 +31,11 @@ public class UserDao {
         }
     }
 
+    /**
+     * Methode um einen vorhandenen Nutzer zu bearbeiten
+     *
+     * @param user Ein Nutzer, der bereits in der Datenbank ist und geupdated werden soll
+     */
     public void updateUser(User user) {
         try (Session session = HibernateUtils.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
@@ -36,6 +49,12 @@ public class UserDao {
         }
     }
 
+    /**
+     * Methode um einen User aus der Datenbank zu erhalten mit einem gegebenen Username
+     *
+     * @param username Ein Username, der in der Datenbank gesucht werden soll
+     * @return User bei einem gefundenen User, falls kein User gefunden wurde, wird null zurückgegeben
+     */
     public User getUser(String username) {
         try (Session session = HibernateUtils.getSessionFactory().openSession()) {
 //            Query query = session.createQuery("SELECT u FROM User u WHERE u.username=:name");
@@ -49,6 +68,11 @@ public class UserDao {
         }
     }
 
+    /**
+     * Methode um alle User zu erhalten
+     *
+     * @return List<User> gibt alle User in der Datenbank als Liste aus
+     */
     public List<User> getScoreboard() {
         try (Session session = HibernateUtils.getSessionFactory().openSession()) {
             return session.createQuery("From User ORDER BY score DESC, wins DESC", User.class).list();
