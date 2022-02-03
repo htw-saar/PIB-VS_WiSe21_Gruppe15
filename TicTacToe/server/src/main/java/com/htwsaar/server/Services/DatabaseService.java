dd9a16f553c0cdc3c764b80bd459fed3d32d3106
Bricker17 <qhtrrA7EBaxCm7Z>
2022-02-03 20:36:15 +0100

    ADDED JavaDoc for DatabaseService

--- TicTacToe/server/src/main/java/com/htwsaar/server/Services/DatabaseService.java
+++ TicTacToe/server/src/main/java/com/htwsaar/server/Services/DatabaseService.java
@@ -7,22 +7,43 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
+/**
+ * Eine Serviceklasse um Datenbank Operationen zu vereinfachen
+ */
 public class DatabaseService {
     private final Logger logger = LogManager.getLogger(DatabaseService.class);
     private UserDao userDao;
 
+    /**
+     * Konstruktor der die Datenbank initialisiert
+     */
     public DatabaseService() {
         initDatabase();
     }
 
+    /**
+     * Eine Methode um die UserDao in eine Variable zu schreiben
+     */
     private void initDatabase() {
         userDao = new UserDao();
     }
 
+    /**
+     * Eine Methode um einen User aus der Datenbank zu erhalten mit gegebenem Username
+     * @param username Der Username des zu suchenden Users
+     * @return User wenn ein User gefunden wurde, null wenn kein User gefunden wurde
+     */
     public User getUserData(String username) {
         return userDao.getUser(username);
     }
 
+    /**
+     * Eine Methode um Loses und Wins zu ändern
+     *
+     * @param user Ein User, in dem Wins/Loses bearbeitet werden sollen
+     * @param additionalWins Zahl der zu addierenden Wins
+     * @param additionalLoses Zahl der zu addierenden Loses
+     */
     private void updateLosesAndWins(User user, int additionalWins, int additionalLoses) {
         int score = user.getScore();
         if (additionalWins > additionalLoses) {
@@ -39,6 +60,12 @@ public class DatabaseService {
         userDao.updateUser(user);
     }
 
+    /**
+     * Eine Methode um einen neuen Nutzer anzulegen
+     *
+     * @param username Der Username eines neuen Nutzers
+     * @param password Das Passwort eines neuen Nutzers
+     */
     public void addUser(String username, String password) {
         if (username != null && !username.isEmpty()) {
             if (userDao.getUser(username) == null) {
@@ -57,16 +84,33 @@ public class DatabaseService {
 
     }
 
+    /**
+     * Eine Methode um einem Nutzer einen Wins zu addieren
+     *
+     * @param username Der Username, dem ein Win addiert werden soll
+     */
     public void addWin(String username) {
         User user = getUserData(username);
         updateLosesAndWins(user, 1, 0);
     }
 
+    /**
+     * Eine Methode um einem Nutzer einen Loose zu addieren
+     *
+     * @param username Der Username, dem ein Loose addiert werden soll
+     */
     public void addLose(String username) {
         User user = getUserData(username);
         updateLosesAndWins(user, 0, 1);
     }
 
+    /**
+     * Eine Methode um das Passwort eines Benutzers zu ändern
+     *
+     * @param username Der Username des zu bearbeitenden Users
+     * @param oldPassword Das alte Passwort
+     * @param newPassword Das neue Passwort
+     */
     public void changePassword(String username, String oldPassword, String newPassword) {
         if (newPassword != null && !newPassword.isEmpty()) {
             if (oldPassword != null && !oldPassword.isEmpty()) {
@@ -80,6 +124,11 @@ public class DatabaseService {
 
     }
 
+    /**
+     * Eine Methode um alle Benutzer auszugeben
+     *
+     * @return Gibt alle Benutzer aus
+     */
     public List<User> getScoreboard() {
         return userDao.getScoreboard();
     }
