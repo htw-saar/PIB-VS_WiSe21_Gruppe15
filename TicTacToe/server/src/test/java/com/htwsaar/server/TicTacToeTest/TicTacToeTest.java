820ffab103e2235cc553e52699f9fdb85c3b22cf
Hamudi <AhmadAnwar@web.de>
2022-03-15 16:51:12 +0100

    @ Test Empty

---
+++ TicTacToe/server/src/test/java/com/htwsaar/server/TicTacToeTest/TicTacToeTest.java
@@ -0,0 +1,34 @@
+package com.htwsaar.server.TicTacToeTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+import com.htwsaar.server.Game.TicTacToe;
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    //@Test
+   // public void StringTest(){
+        //var testobj = new TicTacToe("King");
+       // assertEquals("King",testobj.getActivePlayer());
+
+
+    //}
+   //public void enumtests(){
+       // assertTrue(true);
+    //}
+
+
+
+    }
+
+
+
