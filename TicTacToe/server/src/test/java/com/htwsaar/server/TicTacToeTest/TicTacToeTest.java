67395c1696e386f2c4c6e0026819bdeaf31f96ba
ToxiqVipeZ <g.-oli@t-online.de>
2022-03-16 13:42:41 +0100

    -more Tests

--- TicTacToe/server/src/test/java/com/htwsaar/server/TicTacToeTest/TicTacToeTest.java
+++ TicTacToe/server/src/test/java/com/htwsaar/server/TicTacToeTest/TicTacToeTest.java
@@ -2,6 +2,8 @@ package com.htwsaar.server.TicTacToeTest;
 
 
 import com.htwsaar.server.Game.TicTacToe;
+import com.htwsaar.server.Hibernate.dao.UserDao;
+import com.htwsaar.server.Hibernate.entity.User;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,7 +12,7 @@ public class TicTacToeTest {
     TicTacToe serverLogic = new TicTacToe("testuser");
 
     @Test
-    public void shouldAnswerWithTrue() {
+    public void shouldAnswerWithTrueTest() {
         assertTrue(true);
     }
 
@@ -32,23 +34,47 @@ public class TicTacToeTest {
     }
 
     @Test
-    public void serverGameInitTestfalse(){
+    public void serverGameInitTestFalse() {
         assertNotEquals("Toni", serverLogic.getActivePlayer());
     }
 
-
-
     @Test
-    public void EmptyGameBoard(){
+    public void EmptyGameBoardTest() {
         assertNotNull(serverLogic.getGameboard());
     }
 
     @Test
-    public void noJoinCode(){
+    public void noJoinCodeTest() {
         assertNotNull(serverLogic.getJoinCode());
     }
 
+    @Test
+    public void setFieldTest() {
+        serverLogic.setField(2);
+        var gameboard = serverLogic.getGameboard();
 
+        assertNotNull(gameboard[2]);
+    }
+
+    @Test
+    public void checkWinConditionTest() {
+        serverLogic.setField(3);
+        serverLogic.setField(4);
+        serverLogic.setField(5);
+
+        var gameboard = serverLogic.getGameboard();
+
+        serverLogic.checkWinCondition(TicTacToe.Winner.Player1);
+        assertEquals(serverLogic.getGameStatus(),serverLogic.checkWinCondition(TicTacToe.Winner.Player1));
+    }
+
+    @Test
+    public void compareJoinCodeTest(){
+        var joinCode = serverLogic.getJoinCode();
+        var comparedJoinCode = serverLogic.compareJoinCode(joinCode);
+
+        assertEquals(comparedJoinCode, 1);
+    }
 
 
 }
