afc2397ab4f4f4feacb368c5968c6ea1b1d9688a
IsAlreadyTaken <Maximilian.Weyand@gmx.de>
2022-03-17 12:18:02 +0100

    Add ServerClient_Connect_Interface.java rematch Funktion

--- TicTacToe/server/src/main/java/com/htwsaar/server/RMI/ServerClient_Connect_Interface.java
+++ TicTacToe/server/src/main/java/com/htwsaar/server/RMI/ServerClient_Connect_Interface.java
@@ -1,35 +1,38 @@
 package com.htwsaar.server.RMI;
 
 import com.htwsaar.server.Game.TicTacToe;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
 public interface ServerClient_Connect_Interface extends Remote {
 
-        Boolean userLoginExists(String name) throws RemoteException;
+    Boolean userLoginExists(String name) throws RemoteException;
 
-        Boolean createLoginData(String name, String password) throws RemoteException;
+    Boolean createLoginData(String name, String password) throws RemoteException;
 
-        Boolean sendLoginData(String name, String password) throws RemoteException;
+    Boolean sendLoginData(String name, String password) throws RemoteException;
 
-        List<String> scoreboardRequest() throws RemoteException;
+    List<String> scoreboardRequest() throws RemoteException;
 
-        String scoreboardRequestForUser(String name) throws RemoteException;
+    String scoreboardRequestForUser(String name) throws RemoteException;
 
-        Boolean createGame(String username) throws  RemoteException;
+    Boolean createGame(String username) throws RemoteException;
 
-        Boolean joinGame(String username, int joinCode) throws RemoteException;
+    Boolean joinGame(String username, int joinCode) throws RemoteException;
 
-        TicTacToe.Winner setField(String username, int pos) throws RemoteException;
+    TicTacToe.Winner setField(String username, int pos) throws RemoteException;
 
-        String getActivePlayer(String username) throws RemoteException;
+    String getActivePlayer(String username) throws RemoteException;
 
-        String[] returnGameboard(String username) throws RemoteException;
+    String[] returnGameboard(String username) throws RemoteException;
 
-        Boolean checkGameStart(String username) throws RemoteException;
+    Boolean checkGameStart(String username, String listTyp) throws RemoteException;
 
-        TicTacToe.Winner getGameStatus(String username) throws RemoteException;
+    TicTacToe.Winner getGameStatus(String username) throws RemoteException;
 
-        int getUserId(String username) throws RemoteException;
-    }
+    int getUserId(String username) throws RemoteException;
+
+    Boolean rematchGame(String username) throws RemoteException;
+}
