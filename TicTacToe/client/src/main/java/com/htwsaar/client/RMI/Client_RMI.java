e0feed68abb6fa5e5c0198925478488ec621661e
IsAlreadyTaken <Maximilian.Weyand@gmx.de>
2022-03-22 12:29:38 +0100

    Mario Dickmann Add JavaDoc Client_RMI.java

--- TicTacToe/client/src/main/java/com/htwsaar/client/RMI/Client_RMI.java
+++ TicTacToe/client/src/main/java/com/htwsaar/client/RMI/Client_RMI.java
@@ -27,17 +27,27 @@ public class Client_RMI {
         logger.debug("Verbindung zum Server wurde erfolgreich hergestellt!");
     }
 
+    /**
+     * Die Methode init_RMI versucht eine VErbindung zum Server herzustellen und
+     * startet einen neuen Versuch alle 2 Sekunden
+     */
     private void init_RMI() {
         while (clientStub == null) {
             clientStub = connectToServer();
             if (clientStub == null) {
                 System.out.println("Verbindung zum Server fehlgeschlagen!");
-                System.out.println("Nächster Versuch startet gleich...");
+                System.out.println("NÃ¤chster Versuch startet gleich...");
                 waitOn(2);
             }
         }
     }
 
+    /**
+     * Eine Methode zum ueberpruefen ob der Benutzer existiert
+     *
+     * @param name der zu ueberpruefende Name
+     * @return true wennn der Benutzer existiert andernfalls false
+     */
     public Boolean userLoginExists(String name) {
         try {
             return clientStub.userLoginExists(name);
@@ -47,6 +57,13 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Eine Methode die einen neuen Benutzer anlegt
+     *
+     * @param name     der Name des neuen Benutzers
+     * @param password das Passwort des neuen Benutzers
+     * @return true wennn der Benutzer erfolgreich angelegt wurde
+     */
     public Boolean createLoginData(String name, String password) {
         try {
             return clientStub.createLoginData(name, password);
@@ -56,6 +73,11 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Gibt den Benutzernamen des eingelogten Spielers zurueck
+     *
+     * @return Der Name des Benutzers
+     */
     public String getLoggedInUser() {
         try {
             return loggedInUser;
@@ -65,7 +87,12 @@ public class Client_RMI {
         }
     }
 
-
+    /**
+     * Gibt die ID eines Spielers zurueck
+     *
+     * @param username Der Name des Benutzers desen ID gesucht wird
+     * @return Die ID des Benutzers
+     */
     public int getUserId(String username) {
         try {
             return clientStub.getUserId(username);
@@ -75,6 +102,12 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Ein Rematch gegen einen Benutzer starten
+     *
+     * @param username Der Name des Benutzers zum rematchen
+     * @return true wenn das Rematch stattfinden kann
+     */
     public Boolean rematchGame(String username) {
         try {
             return clientStub.rematchGame(username);
@@ -84,6 +117,12 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Ein Spiel starten mit dem Benutzer als erster Spieler
+     *
+     * @param username Der Name des Benutzers der Spieler 1 wird
+     * @return der preshared Key
+     */
     public String createGame(String username) {
         try {
             return clientStub.createGame(username);
@@ -93,6 +132,12 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Gibt das Gameboard zurueck an dem der Benutzer teil nimmt
+     *
+     * @param username Der Name des Benutzers dessen Gameboard geladen werden soll
+     * @return das Gameboard als 2D-Array
+     */
     public String[] returnGameboard(String username) {
         try {
             return clientStub.returnGameboard(username);
@@ -102,6 +147,13 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Fuegt den Spieler dem Spiel mit dem passenden join Code hinzu
+     *
+     * @param username Der Spieler der hinzugefuegt werden soll
+     * @param joinCode Der join Code des Spieles
+     * @return der preshared Key
+     */
     public String joinGame(int joinCode, String username) {
         try {
             return clientStub.joinGame(username, joinCode);
@@ -111,6 +163,14 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Setzt das Feld mit dem Symbol des Spielers
+     *
+     * @param pos          Position des zu setzenden Feldes im Gameboard
+     * @param username     Der Benutzer der das Feld setzt
+     * @param preSharedKey zur authentifizierung des Spielers
+     * @return Enum mit dem Game Status
+     */
     public TicTacToe.Winner setField(String username, int pos, String preSharedKey) {
         try {
             return clientStub.setField(username, pos, preSharedKey);
@@ -120,6 +180,13 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Testet die Login Daten
+     *
+     * @param userName Der Benutzername
+     * @param password Das Passwort des Benutzers
+     * @return true wenn die Daten stimmen
+     */
     private boolean testLoginData(String userName, String password) {
         try {
             return clientStub.sendLoginData(userName, password);
@@ -129,6 +196,9 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Gibt das gesamte Scoreboard aus
+     */
     public void ShowScoreBoardAll() {
         try {
             String format = " %2s %12s %2s %6s %2s %6s %2s %6s %2s";
@@ -149,6 +219,12 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Gibt die Statistiken eines einzelnen Benutzers aus
+     *
+     * @param username Der Benutzername desen Statistiken angezeigt werden sollen
+     * @return 1 wenn es funktioniert hat 0 wenn es ein Fehler gab
+     */
     public int ShowOwnStats(String username) {
         try {
             System.out.println(clientStub.scoreboardRequestForUser(username));
@@ -159,6 +235,11 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Baut die Verbindung zum Server auf
+     *
+     * @return Der stub bei einer erfolgreichen Verbindung sonst null
+     */
     private ServerClient_Connect_Interface connectToServer() {
         try {
             Registry registry = LocateRegistry.getRegistry(HOST_IP, PORT);
@@ -171,11 +252,25 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Eine Methode zum einlogen des Benutzers
+     *
+     * @param username Der Benutzername
+     * @param password Das Passwort des Benutzers
+     * @return true wenn die Login Daten stimmen sonst false
+     */
     public Boolean login(String username, String password) {
         loggedInUser = username;
         return testLoginData(username, password);
     }
 
+    /**
+     * Ueberprueft ob das Spield es Benutzers schon gestartet ist
+     *
+     * @param username der Benutzer desen Status ueberprueft werden soll
+     * @param listTyp  Der Name der Liste in der gesucht werden soll
+     * @return true wenn es gestartet ist sonst false
+     */
     public Boolean checkGameStart(String username, String listTyp) {
         try {
             return clientStub.checkGameStart(username, listTyp);
@@ -186,6 +281,12 @@ public class Client_RMI {
 
     }
 
+    /**
+     * Gibt den aktiven Spieler zurueck
+     *
+     * @param username der Benutzer in desen Spiel der aktive Spieler gesucht wird
+     * @return Der Name des aktiven Spielers
+     */
     public String getActivePlayer(String username) {
         try {
             return clientStub.getActivePlayer(username);
@@ -195,6 +296,12 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Gibt den aktiven Spieler zurueck
+     *
+     * @param username der Benutzer in desen Spiel der aktive Spieler gesucht wird
+     * @return Der Name des aktiven Spielers
+     */
     public TicTacToe.Winner getGameStatus(String username) {
         try {
             return clientStub.getGameStatus(username);
@@ -204,6 +311,11 @@ public class Client_RMI {
         }
     }
 
+    /**
+     * Eine Methode um eine gewisse Zeit zu warten
+     *
+     * @param sec Die Anzahl der Sekunden die gewartet werden soll
+     */
     private void waitOn(int sec) {
         try {
             TimeUnit.SECONDS.sleep(sec);
@@ -211,5 +323,4 @@ public class Client_RMI {
             e.printStackTrace();
         }
     }
-
 }
